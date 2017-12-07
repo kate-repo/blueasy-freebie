fb192289dd07b9e946f6eb25855eba32c4ad36d4
hillel-kat <hillel-kate@gmail.com>
2017-12-07 22:04:25 +0200

    Add Gallery plugin for potfolio section.

--- js/script.js
+++ js/script.js
@@ -4,4 +4,12 @@ $(() => {
     $('#form').submit(() => {
         $('#form').find('input, textarea').val('');
     });
+
+    $('.portfolio-item-wrapper').click((event) => {
+        const target = event.target || event.srcElement,
+            link = target.src ? target.parentNode : target,
+            options = {index: link, event: event},
+            links = $(target).parent('a');
+        blueimp.Gallery(links, options);
+    });
 });
