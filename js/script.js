bea99b232fb9dbe53d406c2aaf574bc8aa6aa7b1
Kate Shokolova <katerina.shokolova@genesys.com>
2017-12-13 13:12:27 +0200

    Portfolio: Setup jQuery blueimp-Gallery plugin.

--- js/script.js
+++ js/script.js
@@ -7,9 +7,9 @@ $(() => {
 
     $('.portfolio-item-wrapper').click((event) => {
         const target = event.target || event.srcElement,
-            link = target.src ? target.parentNode : target,
-            options = {index: link, event: event},
-            links = $(target).parent('a');
+            src = target.src ? target.parentNode : target,
+            options = {index: src, event: event, hidePageScrollbars: false},
+            links = $('.portfolio-item-wrapper');
         blueimp.Gallery(links, options);
     });
 });
